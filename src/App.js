485010620e03a671a1ee9998a243f93020e8a779
allen4dev <alanaliagadev@gmail.com>
2017-10-20 10:57:50 -0400

    [Add] basic redux structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import Autocomplete from './containers/Autocomplete';
 
+import store from './store';
+
 const App = () => {
   return (
-    <div className="App">
-      <Autocomplete />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Autocomplete />
+      </div>
+    </Provider>
   );
 };
 
